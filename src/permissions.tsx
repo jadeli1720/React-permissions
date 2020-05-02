214de3490a928daae7d74b4feae1c265b33dc87a
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-05-01 20:17:33 -0600

    defined type of permissions

--- src/permissions.tsx
+++ src/permissions.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+type Permission = "user:write" | "user:read" | "user:admin";
 
-const PermissionsContext = React.createContext(undefined);
+type PermissionsContextValue = {
+    permissions: Permission[],
+}
+
+const PermissionsContext = React.createContext<PermissionsContextValue | null> (null);
 
 export const PermissionsProvider: React.FC = ({ children }) => {
+    /*using state to mock out what an actual authentication system communicating with our backend or Auth0 would look like that contains the token or json that contains the proper permissions*/
+    const [permissions, setPermissions] = 
+        useState<Permission[]>(['user:write', 'user:read']);
+        
     return (
-        <PermissionsContext.Provider value={undefined}>
+        <PermissionsContext.Provider value={{ permissions }}>
             {children}
         </PermissionsContext.Provider>
     )
